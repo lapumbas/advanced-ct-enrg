6e5cd09d75c8afdfa077b5445d09ad12f92cdbd8
lapumbas <lapumbas@ya.ru>
2018-06-29 11:53:32 +0300

    finishing

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
 
 gulp.task("sass", function () {
@@ -11,12 +15,19 @@ gulp.task("sass", function () {
     .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
 
 gulp.task("serve", ["sass"], () => {
   server.init({
-    server: ".",
+    server: {
+      baseDir: ".",
+      index: "form.html"
+    },
+    // server: ".",
     notify: false,
     open: true,
     cors: true,
@@ -27,3 +38,20 @@ gulp.task("serve", ["sass"], () => {
   gulp.watch("*.html").on("change", server.reload);
   gulp.watch("js/**/*.js").on("change", server.reload);
 });
+
+gulp.task("images", function () {
+  gulp.src("img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("webp", function () {
+  gulp.src("img/**/*.{png,jpg}")
+  .pipe(webp({quality: 50}))
+  .pipe(gulp.dest("img"));
+})
+
